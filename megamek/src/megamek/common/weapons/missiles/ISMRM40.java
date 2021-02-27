202547463a148b17d7b1ea6faecfe5b12c89c5fb
Tirak117 <41876783+Tirak117@users.noreply.github.com>
2021-02-26 19:15:48 -0500

    Update ISMRM40.java

--- megamek/src/megamek/common/weapons/missiles/ISMRM40.java
+++ megamek/src/megamek/common/weapons/missiles/ISMRM40.java
@@ -52,7 +52,7 @@ public class ISMRM40 extends MRMWeapon {
     	.setIntroLevel(false)
     	.setUnofficial(false)
         .setTechRating(RATING_C)
-        .setAvailability(RATING_X, RATING_E, RATING_D, RATING_D)
+        .setAvailability(RATING_E, RATING_D, RATING_B, RATING_C)
         .setISAdvancement(2870, 2880, 2890, DATE_NONE, DATE_NONE)
         .setISApproximate(true, false, false,false, false)
         .setPrototypeFactions(F_DC)
